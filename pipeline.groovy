c49699ac3f9c2fa3a732d6f8fe26ecc70678206b
fhabumugisha <fabriceboris@gmail.com>
2022-02-25 14:55:16 +0100

    update pipeline settings

--- pipeline.groovy
+++ pipeline.groovy
@@ -19,6 +19,15 @@ pipeline {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
+                    //}
+                //changed 
+                    emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build", 
+                        attachLog:true, 
+                        compressLog: true, 
+                        to : "fabriceboris@yahoo.fr",
+                        recipientProviders: [upstreamDevelopers(), developers()]
+                        
                 }
             }
         }
